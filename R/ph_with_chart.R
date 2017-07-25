298a0bddbbd1c84fb6ea71d61900d06b0d4780a4
dgohel <david.gohel@ardata.fr>
2017-07-25 19:09:16 +0200

    officer functions

--- R/ph_with_chart.R
+++ R/ph_with_chart.R
@@ -1,20 +1,5 @@
-#' @export
-#' @importFrom stats setNames
-#' @importFrom openxlsx write.xlsx
-#' @importFrom xml2 read_xml xml_find_first xml_replace as_xml_document xml_add_child write_xml
-ph_with_chart <- function( x, value, type = "body", index = 1 ){
+pml_chart <- function(x, value, id_x, id_y){
 
-  stopifnot( type %in% c("ctrTitle", "subTitle", "dt", "ftr", "sldNum", "title", "body") )
-
-  slide <- x$slide$get_slide(x$cursor)
-  xfrm_df <- slide$get_xfrm(type = type, index = index)
-  left = xfrm_df$offx
-  top = xfrm_df$offy
-  width = xfrm_df$cx
-  height = xfrm_df$cy
-
-
-  # filenames and dirs ----
   charts_dir <- file.path(x$package_dir, "ppt/charts")
   xlsx_dir <- file.path(x$package_dir, "ppt/embeddings")
   dir.create(charts_dir, recursive = TRUE, showWarnings = FALSE)
@@ -30,18 +15,19 @@ ph_with_chart <- function( x, value, type = "body", index = 1 ){
 
 
   rel_str <- paste0("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>",
-  "<Relationships  xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\"><Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/package\" Target=\"../embeddings/%s\"/></Relationships>")
+                    "<Relationships  xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\"><Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/package\" Target=\"../embeddings/%s\"/></Relationships>")
   rel_str <- sprintf( rel_str, basename(xlsx_file) )
   cat(rel_str, file = rel_filename)
 
   write.xlsx(value$data_series, file = xlsx_file, sheetName = "sheet1")
-  xml_elt <- format(value, id_x = "64451712", id_y = "64453248")
+  xml_elt <- format(value, id_x = id_x, id_y = id_y)
 
   node <- xml_find_first(xml_doc, "//c:plotArea")
   xml_replace( node, as_xml_document(xml_elt) )
 
   write_xml(xml_doc, chart_file)
 
+  slide <- x$slide$get_slide(x$cursor)
   next_id <- slide$relationship()$get_next_id()
   slide$relationship()$add(paste0("rId", next_id),
                            type = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/chart",
@@ -60,8 +46,8 @@ ph_with_chart <- function( x, value, type = "body", index = 1 ){
     "<p:nvPr/>",
     "</p:nvGraphicFramePr>",
     "<p:xfrm rot=\"0\">",
-    sprintf( "<a:off x=\"%.0f\" y=\"%.0f\"/>", left*914400, top*914400 ),
-    sprintf( "<a:ext cx=\"%.0f\" cy=\"%.0f\"/>", width*914400, height*914400 ),
+    "<a:off x=\"0\" y=\"0\"/>",
+    "<a:ext cx=\"0\" cy=\"0\"/>",
     "</p:xfrm>",
     "<a:graphic>",
     "<a:graphicData uri=\"http://schemas.openxmlformats.org/drawingml/2006/chart\">",
@@ -74,11 +60,35 @@ ph_with_chart <- function( x, value, type = "body", index = 1 ){
   partname <- file.path( "/ppt/charts", basename(chart_file) )
   override <- setNames("application/vnd.openxmlformats-officedocument.drawingml.chart+xml", partname )
   x$content_type$add_override(value = override)
-
-  xml_add_child(xml_find_first(slide$get(), "//p:spTree"), as_xml_document(graphic_frame))
-  slide$save()
-  x$slide$update()
-  x
+  graphic_frame
 }
 
+#' @export
+#' @importFrom stats setNames
+#' @importFrom openxlsx write.xlsx
+#' @importFrom xml2 read_xml xml_find_first xml_replace as_xml_document xml_add_child write_xml
+#' @title add chart into a PowerPoint slide
+#' @description add a chart as a new shape in the current slide.
+#' @param x a pptx device
+#' @param value \code{ms_chart} object
+#' @param type placeholder type
+#' @param index placeholder index (integer). This is to be used when a placeholder type
+#' is not unique in the current slide, e.g. two placeholders with type 'body'.
+#' @importFrom officer ph_from_xml
+ph_with_chart <- function( x, value, type = "body", index = 1 ){
+  stopifnot(inherits(x, "rpptx"))
+  graphic_frame <- pml_chart(x, value, id_x = "64451712", id_y = "64453248")
+  ph_from_xml(x = x, value = graphic_frame, type = type, index = index )
+}
+
+#' @export
+#' @param left,top location of chart on the slide
+#' @param height,width Height and width in inches.
+#' @rdname ph_with_chart
+#' @importFrom officer ph_from_xml_at
+ph_with_chart_at <- function( x, value, left, top, width, height ){
+  stopifnot(inherits(x, "rpptx"))
+  graphic_frame <- pml_chart(x, value, id_x = "64451712", id_y = "64453248")
+  ph_from_xml_at(x = x, value = graphic_frame, left = left, top = top, width = width, height = height )
+}
 
