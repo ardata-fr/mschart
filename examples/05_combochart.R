49d9ff709f2a730bf907d359928205f6efca8278
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2025-03-27 07:01:42 +0100

    cleanup example

--- examples/05_combochart.R
+++ examples/05_combochart.R
@@ -1,22 +1,17 @@
-library(mschart)
-
-
 # example chart 01 -------
 
 dat <- longley
 dat$Year <- as.Date(paste0(dat$Year, "-01-01"))
 dat$`GNP Deflator` <- dat$GNP.deflator
 
-empl <- dat |>
-  ms_barchart(x = "Year", y = c("Employed", "Unemployed"), asis = TRUE) |>
-  chart_labels(title = "Longley's Economic Regression Data",
-               xlab = "1947 to 1962", ylab = "Employment") |>
-  chart_ax_x(num_fmt = "yyyy") |>
-  as_bar_stack(dir = "vertical")
+empl <- ms_barchart(data = dat, x = "Year", y = c("Employed", "Unemployed"), asis = TRUE)
+empl <- chart_labels(x = empl, title = "Longley's Economic Regression Data",
+                     xlab = "1947 to 1962", ylab = "Employment")
+empl <- chart_ax_x(x = empl, num_fmt = "yyyy")
+empl <- as_bar_stack(x = empl, dir = "vertical")
 
-gdp <- dat |>
-  ms_linechart(x = "Year", y = "GNP Deflator", asis = TRUE) |>
-  chart_labels(ylab = "GNP implicit price deflator (1954 = 100)") |>
-  chart_ax_y(second_axis = TRUE)
+gdp <- ms_linechart(data = dat, x = "Year", y = "GNP Deflator", asis = TRUE)
+gdp <- chart_labels(x = gdp, ylab = "GNP implicit price deflator (1954 = 100)")
+gdp <- chart_ax_y(x = gdp, second_axis = TRUE)
 
 cb <- ms_combochart(empl, gdp)
